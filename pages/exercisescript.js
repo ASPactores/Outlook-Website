630cfb65f3f7472cd623432b6bdf1384caadd24a
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2022-01-05 11:57:19 +0800

    Responsive Exercise Page Added

---
+++ pages/exercisescript.js
@@ -0,0 +1,11 @@
+function dropdownApp() {
+    document.getElementById("navtext-content").classList.toggle("show");
+}
+
+function show() {
+    document.getElementById("test-content").classList.toggle("active");
+}
+
+function move() {
+    document.getElementById("toggle").classList.toggle("move-left");
+}
